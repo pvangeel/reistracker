67e3fc09734f9463fc94e4d7984bb2de3c0cd583
Pieter Van Geel <pieter.vangeel@gmail.com>
2014-01-29 21:09:03 +0100

    - Included templates - Added Velocity dependencies + config

--- src/main/java/org/goldflower/config/ApplicationConfig.java
+++ src/main/java/org/goldflower/config/ApplicationConfig.java
@@ -2,6 +2,8 @@ package org.goldflower.config;
 
 import static org.springframework.context.annotation.ComponentScan.Filter;
 
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +12,10 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 
 import org.goldflower.Application;
+import org.springframework.ui.velocity.VelocityEngineFactory;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackageClasses = Application.class, excludeFilters = @Filter({Controller.class, Configuration.class}))
@@ -21,5 +27,15 @@ class ApplicationConfig {
 		ppc.setLocation(new ClassPathResource("/persistence.properties"));
 		return ppc;
 	}
+
+    @Bean
+    public VelocityEngine getVelocityEngine() throws VelocityException, IOException {
+        VelocityEngineFactory factory = new VelocityEngineFactory();
+        Properties props = new Properties();
+        props.put("resource.loader", "class");
+        props.put("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        factory.setVelocityProperties(props);
+        return factory.createVelocityEngine();
+    }
 	
 }
